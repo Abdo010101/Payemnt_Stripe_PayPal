d6db8c4d44916a05470dac3bf4208faa8fd4b796
Abdo010101 <abdelmenem.developer@gmail.com>
2024-06-21 17:30:19 +0300

    sdfsdf

--- lib/core/utils/service/api_keys.dart
+++ lib/core/utils/service/api_keys.dart
@@ -1,7 +1,5 @@
 class ApiKeys {
-  static String secretToken =
-      'sk_test_51PAx2B2NEwbDLKsOtPVMC9deEGAyRMCUFqmJCJQHHMNjfi5AkX9y4WxNvACOU2HJokPelBxzotQegFsecip1Z96o007IFblOCc';
+  static String secretToken = '';
 
-  static String publishKey =
-      'pk_test_51PAx2B2NEwbDLKsOFsKXiaE96z1eZxaCz5xytIjLueDTsqErIfFBJS31fdwba6EY6IGOJb6NIiddZNZU4jfGw3Jo00KDlrUKGu';
+  static String publishKey = '';
 }
